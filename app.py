d5ebaa4211e412b6505522e25e866873ac398641
myths-047 <manas.manjhi07@gmail.com>
2025-12-26 13:41:32 +0530

    Home route and Dashboard Template

--- app.py
+++ app.py
@@ -20,23 +20,51 @@ def init_db():
                         name TEXT NOT NULL,
                         spending_limit REAL
                     )
-                    """)
+                """)
     
     cursor.execute("""
                     CREATE TABLE IF NOT EXISTS expenses (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         category_id INTEGER,
-                        descripton TEXT NOT NULL,
+                        date TEXT NOT NULL,
+                        description TEXT NOT NULL,
                         amount REAL NOT NULL,
                         FOREIGN KEY(category_id) REFERENCES categories(id)
                     )
-                    """)
+                """)
     
     conn.commit()
     conn.close()
 
 init_db()
 #===================================================================================
+
+@app.route('/')
+def index():
+    conn=get_db_connection()
+
+    expenses=conn.execute("""
+            SELECT expenses.*,categories.name as category_name
+            FROM expenses
+            LEFT JOIN categories ON expenses.category_id=categories.id    
+    """).fetchall()
+
+    categories=conn.execute("""SELECT * FROM categories""").fetchall()
+
+    total_spent=conn.execute("""
+            SELECT SUM(amount) as total 
+            FROM expenses
+    """).fetchone()
+
+    conn.close()
+
+    total=total_spent['total'] if total_spent['total'] else 0
+
+    return render_template('index.html', 
+                    expenses=expenses, 
+                    categories=categories,
+                    total_spent=total)
+
 
 if __name__=="__main__":
     app.run(debug=True)
