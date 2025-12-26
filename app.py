f22f59d9dd3e336dfe12a9c3638ce6210c725113
myths-047 <manas.manjhi07@gmail.com>
2025-12-26 14:29:37 +0530

    Add Expense Functionality added

--- app.py
+++ app.py
@@ -65,6 +65,44 @@ def index():
                     categories=categories,
                     total_spent=total)
 
+@app.route('/add_category',methods=['GET','POST'])
+def category():
+    if request.method=='POST':
+        name=request.form["name"]
+        spending_limit=request.form["spending_limit"]
+
+        conn=get_db_connection()
+        conn.execute("INSERT INTO categories (name,spending_limit) VALUES(?,?)",(name,spending_limit))
+        
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('index'))
+    
+    return render_template('add_categories.html')
+
+@app.route('/add_expense',methods=['GET','POST'])
+def add_expenses():
+    conn=get_db_connection()
+
+    if request.method=='POST':
+        category_id=request.form['category_id']
+        date=request.form['date']
+        description=request.form['description']
+        amount=request.form['amount']
+
+        conn.execute("INSERT INTO expenses (category_id,date,description,amount) VALUES(?,?,?,?)",
+                    (category_id,date,description,amount))
+        
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('index'))
+    
+    categories=conn.execute("SELECT * FROM categories").fetchall()
+    conn.close()
+
+    return render_template('add_expense.html',categories=categories)
 
 if __name__=="__main__":
     app.run(debug=True)
